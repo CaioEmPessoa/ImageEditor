fb41513711f7a7c69e3ccc22c64eca61695c32b1
yatomikazuchi <caioevilabner@gmail.com>
2024-06-19 20:04:39 -0300

    updated

---
+++ src/code.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title("Image Viewer")
+root.iconbitmap('C:/Users/User/Documents/programa acao/ImageViewer/images/icon.ico')
+
+
+
+root.mainloop()
