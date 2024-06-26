77b8a5ed8dbdce791ed21ae1d63f03651071be54
yatomikazuchi <caioevilabner@gmail.com>
2024-06-26 17:43:11 -0300

    updated

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ i = 0
 
 status = Label(root, text=f"image {i + 1} of {leng + 1}")
 
-def frontF():
+def screen():
     global image
     global back
     global front
@@ -26,13 +26,15 @@ def frontF():
     global leng
     global status
 
-    i += 1
     image.grid_forget()
     open_img = ImageTk.PhotoImage(Image.open(img_list[i]))
     image = Label(image=open_img)
-    back = Button(text="<", command=backF)
     leave = Button(text="Close", command=exit)
     status = Label(root, text=f"image {i + 1} of {leng + 1}")
+    if i == 0:
+        back = Button(root, text="<", state=DISABLED)
+    else:
+        back = Button(text="<", command=backF)
     if i == leng:
         front = Button(root, text=">", state=DISABLED)
     else:
@@ -45,32 +47,15 @@ def frontF():
     status.grid(row=2, column=2, sticky=E)
 
 
-def backF():
-    global image
-    global back
-    global front
+def frontF():
     global i
-    global img_list
-    global open_img
-    global status
+    i += 1
+    screen()
 
+def backF():
+    global i
     i -= 1
-    image.grid_forget()
-    open_img = ImageTk.PhotoImage(Image.open(img_list[i]))
-    image = Label(image=open_img)
-    leave = Button(text="Close", command=exit)
-    front = Button(text=">", command=frontF)
-    status = Label(root, text=f"image {i + 1} of {leng + 1}")
-    if i == 0:
-        back = Button(root, text="<", state=DISABLED)
-    else:
-        back = Button(text="<", command=backF)
-
-    image.grid(row=0, column=0, columnspan= 3)
-    back.grid(row=1, column=0)
-    leave.grid(row=1, column=1)
-    front.grid(row=1, column=2)
-    status.grid(row=2, column=2, sticky=E)
+    screen()
     
 open_img = ImageTk.PhotoImage(Image.open(img_list[i]))
 
