4bbcbe45afbe5b1e4cc65a170c1c04d61bb977ad
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-07-05 15:18:01 -0300

    Added classes to ImageViews

--- views/imageViews.py
+++ views/imageViews.py
@@ -3,100 +3,102 @@ from PIL import ImageTk, Image
 import customtkinter as ctk
 import os
 
-class 
+class fullView(Tk):
+    def __init__(self):
+        super().__init__()
 
-root = Tk()
-root.title("Image Editor")
-root.geometry('1080x720')
+        self.title("Image Editor")
+        self.geometry('1080x720')
 
-root.columnconfigure((0,1,2,3,4), weight = 1, uniform = 'a')
-root.rowconfigure(0, weight = 1)
+        self.columnconfigure((0,1,2,3,4), weight = 1, uniform = 'a')
+        self.rowconfigure(0, weight = 1)
 
-location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-folder = f"{location}\\images\\"
-icon = f"{location}\\src\\icon.ico"
-root.iconbitmap(icon)
+        location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        APP_ICON = f"{location}\\..\\images\\icon.ico"
+        self.iconbitmap(APP_ICON)
+        
+        self.IMAGES_FOLDER = f"{location}\\..\\images\\"
 
-img_list = [folder + img for img in os.listdir(folder)]
-leng = len(img_list) - 1
-i = 0
 
-ImageFrame = Canvas(root, background= 'black', bd=0, highlightthickness=0, relief='ridge')
-ButtonFrame = Frame(root)
+        self.img_list = [self.IMAGES_FOLDER + img for img in os.listdir(self.IMAGES_FOLDER)]
+        self.img_index = 0 # was 'i'
+        self.img_lenght = len(self.img_list) - 1 # was 'leng'
 
-def img_update():
-    global resized_tk
+        self.viewElements()
 
-    image_original = Image.open(img_list[i])
-    ImageFrame_ratio = image_original.width / image_original.height
+    def img_update(self):
+        global resized_tk
 
-    frame_width = ImageFrame.winfo_width()
-    frame_height = ImageFrame.winfo_height()
+        image_original = Image.open(self.img_list[self.img_index])
+        ImageFrame_ratio = image_original.width / image_original.height
 
-    if frame_width / frame_height > ImageFrame_ratio:
-        width = int(frame_height * ImageFrame_ratio)
-        height = frame_height
-    else:
-        height = int(frame_width / ImageFrame_ratio)
-        width = frame_width
+        frame_width = self.ImageFrame.winfo_width()
+        frame_height = self.ImageFrame.winfo_height()
 
-    resized_image = image_original.resize((width, height))
-    resized_tk = ImageTk.PhotoImage(resized_image)
+        if frame_width / frame_height > ImageFrame_ratio:
+            width = int(frame_height * ImageFrame_ratio)
+            height = frame_height
+        else:
+            height = int(frame_width / ImageFrame_ratio)
+            width = frame_width
 
-    ImageFrame.delete("all")
-    ImageFrame.create_image(frame_width//2, frame_height//2, anchor='center', image=resized_tk)
+        resized_image = image_original.resize((width, height))
+        resized_tk = ImageTk.PhotoImage(resized_image)
 
-    status.config(text=f"Image {i + 1} of {leng + 1}")
+        self.ImageFrame.delete("all")
+        self.ImageFrame.create_image(frame_width//2, frame_height//2, anchor='center', image=resized_tk)
 
-def buttons_update():
-    if i == 0:
-        back.configure(state=DISABLED, fg_color='red')
-    else:
-        back.configure(state=NORMAL, fg_color='blue')
+        self.status.config(text=f"Image {self.img_index + 1} of {self.img_index + 1}")
 
-    if i == leng:
-        front.configure(state=DISABLED, fg_color='red')
-    else:
-        front.configure(state=NORMAL, fg_color='blue')
+    def buttons_update(self):
+        if self.img_index == 0:
+            self.back.configure(state=DISABLED, fg_color='red')
+        else:
+            self.back.configure(state=NORMAL, fg_color='blue')
 
-def frontF():
-    global i
-    i += 1
-    buttons_update()
-    img_update()
-    
-def backF():
-    global i
-    i -= 1
-    buttons_update()
-    img_update()
-    
-image_original = Image.open(img_list[i])
-image_ratio = image_original.size[0] / image_original.size[1]
+        if self.img_index == self.img_lenght:
+            self.front.configure(state=DISABLED, fg_color='red')
+        else:
+            self.front.configure(state=NORMAL, fg_color='blue')
 
-image_tk = ImageTk.PhotoImage(image_original)
+    def frontF(self):
+        self.img_index += 1
+        self.buttons_update()
+        self.img_update()
+        
+    def backF(self):
+        self.img_index -= 1
+        self.buttons_update()
+        self.img_update()
+        
+    def viewElements(self):
+        ## TODO: CHANGE GRID
+        self.ImageFrame = Canvas(self, background= 'black', bd=0, highlightthickness=0, relief='ridge')
+        self.ButtonFrame = Frame(self)
 
-ImageFrame.bind('<Configure>', lambda event: img_update())
+        image_original = Image.open(self.img_list[self.img_index])
+        image_ratio = image_original.size[0] / image_original.size[1] # not used
 
-holder = ctk.CTkLabel(ButtonFrame, text=" ")
-holder.pack(pady=40)
+        image_tk = ImageTk.PhotoImage(image_original) # not used
 
-back = ctk.CTkButton(ButtonFrame, text="<", command=backF)
-back.pack(pady=10)
+        self.ImageFrame.bind('<Configure>', lambda event: self.img_update())
 
-front = ctk.CTkButton(ButtonFrame, text=">", command=frontF)
-front.pack(pady=10)
+        holder = ctk.CTkLabel(self.ButtonFrame, text=" ")
+        holder.pack(pady=40)
 
-#repeated code
-buttons_update() 
+        self.back = ctk.CTkButton(self.ButtonFrame, text="<", command=lambda: self.backF())
+        self.back.pack(pady=10)
 
-status = Label(ButtonFrame, text=f"image {i + 1} of {leng + 1}")
-status.pack(pady=10)
+        self.front = ctk.CTkButton(self.ButtonFrame, text=">", command=lambda: self.frontF())
+        self.front.pack(pady=10)
 
-leave = ctk.CTkButton(ButtonFrame, text="Close app", command=exit)
-leave.pack(pady=200)
+        self.buttons_update() 
 
-ImageFrame.grid(row=0, column=2, columnspan=3, sticky= NSEW)
-ButtonFrame.grid(row=0, column=0, sticky= NSEW)
+        self.status = Label(self.ButtonFrame, text=f"image {self.img_index + 1} of {self.img_lenght + 1}")
+        self.status.pack(pady=10)
 
-root.mainloop()
+        leave = ctk.CTkButton(self.ButtonFrame, text="Close app", command=exit)
+        leave.pack(pady=200)
+
+        self.ImageFrame.grid(row=0, column=2, columnspan=3, sticky= NSEW)
+        self.ButtonFrame.grid(row=0, column=0, sticky= NSEW)
